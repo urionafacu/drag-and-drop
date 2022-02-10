d87f2b75adbdbb60ecc11f961bb94ae0042e1efd
Facu <urionafacu@gmail.com>
2022-02-10 18:19:04 -0300

    feat(main): init project :rocket:

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
+import * as React from 'react';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import { initialTasks } from './utils/tasks';
+import { reorder } from './utils/reorder';
 import './App.css';
 
 function App() {
+  const [tasks, setTasks] = React.useState(initialTasks);
+
+  const handleOnDragEnd = (e) => {
+    const { source, destination } = e;
+    if (!destination) {
+      return;
+    }
+    if (
+      source.index === destination.index &&
+      source.droppableId === destination.droppableId
+    ) {
+      return;
+    }
+    setTasks((prevTasks) => reorder(prevTasks, source.index, destination.index));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DragDropContext onDragEnd={handleOnDragEnd}>
+      <div className="app">
+        <h1>ESTUDIAR</h1>
+        <Droppable droppableId='tasks'>
+          {({ droppableProps, innerRef, placeholder }) => (
+            <ul 
+              {...droppableProps}
+              ref={innerRef}
+              className='task-container'
+            >
+              {tasks.map((e, i) => (
+                <Draggable key={e.id} draggableId={e.id} index={i}>
+                  {({ draggableProps, innerRef, dragHandleProps }) => (
+                    <li
+                      {...draggableProps}
+                      {...dragHandleProps}
+                      ref={innerRef}
+                      className='task-item'
+                    >
+                      {e.text}
+                    </li>
+                  )}
+                </Draggable>
+              ))}
+              {placeholder}
+            </ul>
+          )}
+        </Droppable>
+      </div>
+    </DragDropContext>
   );
 }
 
---
+++ src/utils/tasks.js
@@ -0,0 +1,18 @@
+export const initialTasks = [
+  {
+    id: '1',
+    text: 'Rust',
+  },
+  {
+    id: '2',
+    text: 'DeFi',
+  },
+  {
+    id: '3',
+    text: 'BlockChain',
+  },
+  {
+    id: '4',
+    text: 'Flutter',
+  },
+];
